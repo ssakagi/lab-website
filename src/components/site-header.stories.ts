19ca25bdd1062120b7b0169995792eeca443907e
sakagi <sakagi1121@outlook.jp>
2025-08-13 21:12:13 +0900

    refactor: usePathnameの呼び出しをpropsの受け取りに変更

--- src/components/site-header.stories.ts
+++ src/components/site-header.stories.ts
@@ -11,4 +11,26 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Theme: Story = {
+  args: {
+    pageName: "研究概要",
+  },
+};
+
+export const Publications: Story = {
+  args: {
+    pageName: "発表論文",
+  },
+};
+
+export const Members: Story = {
+  args: {
+    pageName: "メンバー",
+  },
+};
+
+export const Application: Story = {
+  args: {
+    pageName: "院生募集",
+  },
+};
--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,10 +1,10 @@
-import Nav from "./nav";
+import Link from "next/link";
 
-export default function SiteHeader() {
+export default function SiteHeader({ pageName }: { pageName?: string }) {
   return (
     <div className="px-[80px] pt-[60px] pb-[24px] bg-surface-primary">
       <Logo />
-      <Nav />
+      <Nav pageName={pageName} />
     </div>
   );
 }
@@ -17,5 +17,34 @@ function Logo() {
       </div>
       <div className="font-serif font-semibold text-[40px]">生体応答学分野</div>
     </>
+  );
+}
+
+const navList = [
+  { id: 0, name: "研究概要", path: "/theme" },
+  { id: 1, name: "発表論文", path: "/publications" },
+  { id: 2, name: "メンバー", path: "/members" },
+  { id: 3, name: "院生募集", path: "/application" },
+];
+
+// Memo: navListをpageListとして共通化しつつヘッダーやパンくずをクライアントコンポーネントとしてlayout.tsxに含めてもいいが今回はシンプルにページ単位で静的にレンダリングする
+function Nav({ pageName }: { pageName?: string }) {
+  return (
+    <ul className="flex gap-[40px] justify-end">
+      {navList.map((item) => {
+        // インラインではなく変数としてテンプレート文字列にvariantを埋め込んでも正しいcssが反映されない...仕方ないので完全名で指定する
+        const className =
+          item.name === pageName
+            ? "text-nav-label-primary-active nav-label-active"
+            : "text-nav-label-primary-default nav-label-default";
+        return (
+          <li key={item.id}>
+            <Link href={item.path}>
+              <span className={className}>{item.name}</span>
+            </Link>
+          </li>
+        );
+      })}
+    </ul>
   );
 }
