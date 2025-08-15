2a707f8b7c697351c452abae6d47daa1b653d7f1
sakagi <sakagi1121@outlook.jp>
2025-08-15 15:10:50 +0900

    fix: マークアップを修正

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -2,21 +2,27 @@ import Link from "next/link";
 
 export default function SiteHeader({ pageName }: { pageName?: string }) {
   return (
-    <div className="px-[80px] pt-[60px] pb-[24px] bg-surface-primary">
+    <header className="px-[80px] pt-[60px] pb-[24px] bg-surface-primary">
       <Logo />
       <Nav pageName={pageName} />
-    </div>
+    </header>
   );
 }
 
 function Logo() {
   return (
-    <>
-      <div className="font-serif font-semibold text-[24px]">
-        京都大学生命科学研究科高次生命科学専攻
-      </div>
-      <div className="font-serif font-semibold text-[40px]">生体応答学分野</div>
-    </>
+    <h1>
+      <Link href="/">
+        <div className="inline-block">
+          <div className="font-serif font-semibold text-[24px]">
+            京都大学生命科学研究科高次生命科学専攻
+          </div>
+          <div className="font-serif font-semibold text-[40px]">
+            生体応答学分野
+          </div>
+        </div>
+      </Link>
+    </h1>
   );
 }
 
@@ -30,21 +36,23 @@ const navList = [
 // Memo: navListをpageListとして共通化しつつヘッダーやパンくずをクライアントコンポーネントとしてlayout.tsxに含めてもいいが今回はシンプルにページ単位で静的にレンダリングする
 function Nav({ pageName }: { pageName?: string }) {
   return (
-    <ul className="flex gap-[40px] justify-end">
-      {navList.map((item) => {
-        // インラインではなく変数としてテンプレート文字列にvariantを埋め込んでも正しいcssが反映されない...仕方ないので完全名で指定する
-        const className =
-          item.name === pageName
-            ? "text-nav-label-primary-active nav-label-active"
-            : "text-nav-label-primary-default nav-label-default";
-        return (
-          <li key={item.id}>
-            <Link href={item.path}>
-              <span className={className}>{item.name}</span>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+    <nav>
+      <ul className="flex gap-[40px] justify-end">
+        {navList.map((item) => {
+          // インラインではなく変数としてテンプレート文字列にvariantを埋め込んでも正しいcssが反映されない...仕方ないので完全名で指定する
+          const className =
+            item.name === pageName
+              ? "text-nav-label-primary-active nav-label-active"
+              : "text-nav-label-primary-default nav-label-default";
+          return (
+            <li key={item.id}>
+              <Link href={item.path}>
+                <span className={className}>{item.name}</span>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   );
 }
