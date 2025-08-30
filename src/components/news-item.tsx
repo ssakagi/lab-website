50c608a759a3c9dc16164a8246606fb974d46778
sakagi <sakagi1121@outlook.jp>
2025-08-30 19:24:22 +0900

    fix: flex直下のアイテム幅が適切に反映されるよう修正

--- src/components/news-item.tsx
+++ src/components/news-item.tsx
@@ -12,7 +12,7 @@ export default function NewsItem({
   return (
     <Wrapper where={where}>
       <div className="flex gap-[40px] py-[20px] border-b-[1px] border-separator-primary">
-        <div className="w-[128px] text-when-label-primary when-label-md">
+        <div className="flex-none w-[128px] text-when-label-primary when-label-md">
           {when}
         </div>
         <div className="font-sans font-[350] text-[20px] leading-[28px]">
